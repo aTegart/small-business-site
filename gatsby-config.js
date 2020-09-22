b0648fc4aa4cbb278cf2256f80d9a80b6bb2a6a3
aTegart <27782045+aTegart@users.noreply.github.com>
2020-09-21 22:24:01 -0400

    adjust gatsby-config for new page location

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages`,
+        path: `${__dirname}/content/pages`,
       },
     },
     `gatsby-transformer-sharp`,
